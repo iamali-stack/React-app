ae6308a388846665ae3ccdea36bd48b62b8c4867
iamali-stack <icyalikhaled2013@gmail.com>
2025-06-15 12:09:27 +0300

    Update notfound page layout

--- src/NotFound/NotFound.jsx
+++ src/NotFound/NotFound.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import style from './NotFound.module.css';
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <>
+      <div
+        className={`${style.NotFound} d-flex justify-content-center align-items-center text-white flex-column`}
+        style={{ backgroundColor: '#19BC9B', minHeight: '100vh' }}
+      >
+        <h1 className={style.aboutTitle}>Page is Not Found</h1>
+      </div>
+    </>
+  );
 }
